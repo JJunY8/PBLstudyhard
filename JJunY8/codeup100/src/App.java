cfcc670ae356887d1ab22898ebd5571255f3d56f
JJunY8 <sksskdi7718@naver.com>
2022-03-08 15:55:33 +0900

    codeupsolv

--- JJunY8/codeup100/src/App.java
+++ JJunY8/codeup100/src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int n;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello"); // 출력하기01
+        System.out.println("Hello World"); // 출력하기 02
+        System.out.println("Hello\nWorld"); // 출력하기 03
+        System.out.println("\'Hello\'"); // 출력하기 04
+        System.out.println("\"Hello World\""); // 출력하기 05
+        System.out.println("\"!@#$%^&*()\""); // 출력하기 06
+        System.out.println("\"C:\\Download\\hello.cpp\""); // 출력하기 07
+        System.out.println("┌┬┐"); 
+        System.out.println("├┼┤");
+        System.out.println("└┴┘");
+        System.out.println("\u250C\u252C\u2510\n");
+        System.out.println("\u251C\u253C\u2524\n");
+        System.out.println("\u2514\u2534\u2518\n");// 출력하기 08
+
+        n = sc.nextInt();
+        System.out.println(n);//정수 1개 입력받아 그대로 출력하기
     }
 }
