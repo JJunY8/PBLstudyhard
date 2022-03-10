606e84642e8ae974652e8e430e502c00785c492d
jihye05 <89137692+jihye05@users.noreply.github.com>
2022-03-11 01:56:34 +0900

    Delete jihye05 directory

--- jihye05/Num1001.java
+++
@@ -1,8 +0,0 @@
-package CodeUp.src;
-
-public class Num1001 {
-
-    public static void main(String[] args){
-        System.out.println("Hello");
-    }
-}
--- jihye05/Num1006.java
+++
@@ -1,7 +0,0 @@
-package CodeUp.src;
-
-public class Num1006 {
-    public static void main(String[] args){
-        System.out.println("\"!@#$%^&*()\"");
-    }
-}
