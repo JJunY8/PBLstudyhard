409f6c7f1f61745cc161cbfce6a389f31d7fb4a4
iJuYoung <ij961884@gmail.com>
2022-03-11 03:52:10 +0900

    codeup

---
+++ ijy/No14.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class No14 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        float n=sc.nextFloat();
+
+        System.out.printf(String.format("%.2f",n));
+        sc.close();
+    };
+}
---
+++ ijy/No7.java
@@ -0,0 +1,5 @@
+public class No7 {
+    public static void main(String[] args){
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+    };
+}
